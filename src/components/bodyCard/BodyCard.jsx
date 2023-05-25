cb5d613921e0377530000212470c1e434d38c8b5
LucasAraujo639 <lucas.ezequiel.321@gmail.com>
2023-05-25 13:01:38 -0300

    Le cambie el diseño

--- src/components/bodyCard/BodyCard.jsx
+++ src/components/bodyCard/BodyCard.jsx
@@ -1,15 +1,16 @@
+import estilos from './BodyCard.module.css'
 export const BodyCard = ({data}) => {
   return (
     <div className="container text-center ">
-    <div className ='row m-0 '>
-        <div className='col border-top border-bottom border-start border-primary border-2 rounded-1'>Categoría</div>
-        <div className='col border-top border-bottom border-start border-primary border-2 rounded-1'>Fecha</div>
-        <div className='col border border-primary border-2 rounded-1'>Dias Restantes</div>
+    <div className ={`row m-0 ${estilos.contenedor}`}>
+        <div className={`col border-top border-bottom border-start border-primary border-2 rounded-5 border-white text-light ${estilos.fila}`}>Categoría</div>
+        <div className={`col border-top border-bottom border-start border-primary border-2 rounded-5 border-white text-light ${estilos.fila}`}>Fecha</div>
+        <div className={`col border-top border-bottom border-start border-primary border-2 rounded-5 border-white text-light ${estilos.fila}`}>Dias Restantes</div>
     </div>
   <div className='row m-0 '>
   {data && data.map((item,i) => (
             <div key={i} className='row m-0 p-0 align-items-center' >
-                <div className='col '>{item.name}</div>
+                <div className='col  '>{item.name}</div>
                 <div className='col col-5'>{item.fecha}</div>
                 <div className='col '>{item.diasRestantes}</div>
 
